1aceb270d58f9c011a2a14d2adc135b9095b5898
Hannes Davik <Hannes.davik@gmail.com>
2024-10-24 13:33:36 +0200

    Flytta admin menyval in i while-loopen och lade till ändra titel

--- Admin.cs
+++ Admin.cs
@@ -11,16 +11,15 @@ namespace Inloggning
       
        public void AdminView()
        {
-            Console.WriteLine("Gör ett val.\n");
-            Console.WriteLine("1. Skapa användare");
-            Console.WriteLine("2. Byt slumpmässig färg");
-            Console.WriteLine("3. Ändra titel");
-            Console.WriteLine("4. Avsluta");
-
             bool runProgram = true;
 
             while (runProgram)
             {
+                Console.WriteLine("Gör ett val.\n");
+                Console.WriteLine("1. Skapa användare");
+                Console.WriteLine("2. Byt slumpmässig färg");
+                Console.WriteLine("3. Ändra titel");
+                Console.WriteLine("4. Avsluta");
                 string input = Console.ReadLine();
 
                 switch (input)
@@ -32,7 +31,9 @@ namespace Inloggning
 
                         break;
                     case "3":
-
+                        Console.WriteLine("Skriv vad du vill ändra consolefönstrets titel till");
+                        string titel = Console.ReadLine();
+                        Console.Title = titel;
                         break;
 
                     case "4":
